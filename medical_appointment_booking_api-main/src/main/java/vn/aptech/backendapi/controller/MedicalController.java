e33752c621750133012cdf42b4699805021b014e
NguyenHoangAn31 <anphung311002@gmail.com>
2025-02-18 12:00:00 +0700

    Add CRUD APIs for medical records (create, get all, get by ID, update, delete)

--- medical_appointment_booking_api-main/src/main/java/vn/aptech/backendapi/controller/MedicalController.java
+++ medical_appointment_booking_api-main/src/main/java/vn/aptech/backendapi/controller/MedicalController.java
@@ -1,9 +1,15 @@
 package vn.aptech.backendapi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +19,38 @@ import vn.aptech.backendapi.service.Medical.MedicalService;
 @RestController
 @RequestMapping("/api/medical")
 public class MedicalController {
+    
     @Autowired
     private MedicalService medicalService;
+
     @PostMapping("/create")
     public ResponseEntity<MedicalDto> createMedical(@RequestBody MedicalDto medicalDto) {
         System.out.println(medicalDto);
         MedicalDto createdMedical = medicalService.createMedical(medicalDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdMedical);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<MedicalDto>> getAllMedicals() {
+        List<MedicalDto> medicals = medicalService.getAllMedicals();
+        return ResponseEntity.ok(medicals);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MedicalDto> getMedicalById(@PathVariable Long id) {
+        MedicalDto medical = medicalService.getMedicalById(id);
+        return ResponseEntity.ok(medical);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<MedicalDto> updateMedical(@PathVariable Long id, @RequestBody MedicalDto medicalDto) {
+        MedicalDto updatedMedical = medicalService.updateMedical(id, medicalDto);
+        return ResponseEntity.ok(updatedMedical);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteMedical(@PathVariable Long id) {
+        medicalService.deleteMedical(id);
+        return ResponseEntity.noContent().build();
     }
 }
