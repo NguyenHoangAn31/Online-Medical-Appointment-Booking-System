da76035fae2482f9bdec509068c7183382c32506
NguyenHoangAn31 <anphung311002@gmail.com>
2025-02-27 12:00:00 +0700

    feat: Improve Login component with state handling and UI enhancements

--- medical_appointment_booking_web-main/src/pages/Login.jsx
+++ medical_appointment_booking_web-main/src/pages/Login.jsx
@@ -1,43 +1,57 @@
-
-import React, { useState, useEffect } from 'react';
-// import firebase from '../services/auth/firebase';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLoginError = () => {
+    setErrorMessage('Login feature is under development. Please try again later.');
+  };
+
   return (
     <div className='container mt-5'>
-      <div className="row">
-        <div className="col-md-6">
-            <div className="col-12">
-                <img src="/images/image-login.png" alt="" className='img-fluid' />
-            </div>
+      <div className="row align-items-center">
+        {/* Hình ảnh login */}
+        <div className="col-lg-6 d-none d-lg-block">
+          <img src="/images/image-login.png" alt="Login" className='img-fluid' />
         </div>
-        <div className="col-md-6">
-            <div className="col-12">
-              <div className="text-center justify-content-center px-5 mt-xxl-5">
-                  <h3 className='login__title'>Login</h3>
-                  <h5 className='login__title_sup'>Booking appointment</h5>
-                  
-                  <div className='d-flex justify-content-around py-5 border-top border-dark border-2 mt-3 gap-2'>
-                    <motion.div whileTap={{scale: 0.8}}>
-                      <Link to="/login-by-phone" className='btn__login'>Login by Phone Number</Link>
-                    </motion.div>
-                    <motion.div whileTap={{scale: 0.8}}>
-                      <Link to="/login-by-gmail" className='btn__login'>Sign in with gmail account</Link>
-                    </motion.div>                  
-                  </div>
-                  <div className='mt-xl-5'>
-                      <p>Do you have an account yet? <Link to="/register" className='text-decoration-none ms-2'>Register</Link></p>
-                  </div>
 
-              </div>
+        {/* Nội dung login */}
+        <div className="col-lg-6 col-12">
+          <div className="text-center px-4 px-md-5">
+            <h3 className='login__title'>Login</h3>
+            <h5 className='login__title_sup'>Booking appointment</h5>
+
+            {/* Hiển thị thông báo lỗi nếu có */}
+            {errorMessage && <p className="text-danger mt-3">{errorMessage}</p>}
+
+            {/* Nút đăng nhập */}
+            <div className='d-flex flex-column gap-3 py-4 border-top border-dark border-2 mt-3'>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                <Link to="/login-by-phone" className='btn btn-dark w-100'>
+                  Login by Phone Number
+                </Link>
+              </motion.div>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                <Link to="/login-by-gmail" className='btn btn-danger w-100' onClick={handleLoginError}>
+                  Sign in with Gmail Account
+                </Link>
+              </motion.div>
             </div>
+
+            {/* Điều hướng đến trang đăng ký */}
+            <div className='mt-4'>
+              <p>Do you have an account yet? 
+                <Link to="/register" className='text-decoration-none ms-2'>Register</Link>
+              </p>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
