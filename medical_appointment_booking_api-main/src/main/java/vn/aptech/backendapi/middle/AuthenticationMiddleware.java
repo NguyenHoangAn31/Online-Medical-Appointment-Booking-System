8f1a70be7f6f882d8651207f07a55237c9dc07ee
NguyenHoangAn31 <anphung311002@gmail.com>
2025-02-28 12:00:00 +0700

    Add errors and log logs in authenticationMiddleWare

--- medical_appointment_booking_api-main/src/main/java/vn/aptech/backendapi/middle/AuthenticationMiddleware.java
+++ medical_appointment_booking_api-main/src/main/java/vn/aptech/backendapi/middle/AuthenticationMiddleware.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,12 +15,19 @@ import java.io.IOException;
 @Component
 @Order(1)
 @RequiredArgsConstructor
+@Slf4j
 public class AuthenticationMiddleware extends OncePerRequestFilter {
     private final RequestAuthorizer requestAuthorizer;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        requestAuthorizer.tryAuthorizer(request, response);
+        boolean isAuthorized = requestAuthorizer.tryAuthorizer(request, response);
+
+        if (!isAuthorized) {
+            log.warn("Unauthorized request: {} {}", request.getMethod(), request.getRequestURI());
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+            return;
+        }
 
         filterChain.doFilter(request, response);
     }
